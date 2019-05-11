3f36ecc8c0f5ca74ce0b01934a7e2adb49c9f92a
Antonio Proenca <antonio.proenca@capco.com>
2019-05-11 15:46:49 -0300

    Adding field validation.

--- Migrations/20190511181853_Rating.Designer.cs
+++ Migrations/20190511181853_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie2.Models;
 
 namespace RazorPagesMovie2.Migrations
 {
     [DbContext(typeof(RazorPagesMovie2Context))]
-    partial class RazorPagesMovie2ContextModelSnapshot : ModelSnapshot
+    [Migration("20190511181853_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
